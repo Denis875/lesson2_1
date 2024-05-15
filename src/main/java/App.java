6eaccb9c96f51bec51bbc0857dab33151d768cea
denis875 <denisok578@gmail.com>
2024-05-15 20:30:06 +0300

    Logging and code style

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,17 +1,19 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
+    private static final Logger logger = LogManager.getLogger(App.class);
+
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        logger.info(bean.getMessage());
         HelloWorld hw = (HelloWorld) applicationContext.getBean("helloworld");
         Cat cat1 = (Cat) applicationContext.getBean("cat");
         Cat cat2 = (Cat) applicationContext.getBean("cat");
-        System.out.println(bean == hw);
-        System.out.println(cat1 == cat2);
+        logger.info("Equals 2 bean (pattern 'singleton') HelloWorld = " + hw.equals(bean));
+        logger.info("Equals 2 bean (pattern 'prototype') Cat = " + cat1.equals(cat2));
     }
 }
